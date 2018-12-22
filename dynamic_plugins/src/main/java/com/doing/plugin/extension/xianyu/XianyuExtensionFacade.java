20147d9f5436da3cb993078b80db5ed35a7294a0
lidongying <lidongying@corp.netease.com>
2018-12-23 00:00:13 +0800

    add Xianyu activity extension

---
+++ dynamic_plugins/src/main/java/com/doing/plugin/extension/xianyu/XianyuExtensionFacade.java
@@ -0,0 +1,54 @@
+package com.doing.plugin.extension.xianyu;
+
+import com.doing.plugin.BizCode;
+import com.doing.plugin.extension.ExtensionFacade;
+import com.doing.plugin.extension.ModifyExtension1;
+import com.doing.plugin.extension.ModifyExtension2;
+import com.doing.plugin.extension.PreAndPostExtension;
+
+/**
+ * @author Doing on 19/12/2018 19:41
+ */
+@BizCode("jd")
+public class XianyuExtensionFacade implements ExtensionFacade {
+
+    @Override
+    public PreAndPostExtension getPreAndPostExtension() {
+        return new PreAndPostExtension() {
+            @Override
+            public void beforeProcess(String params, StringBuilder processRecord) {
+                System.out.println("闲鱼活动前置处理流程");
+                processRecord.append("京东活动前置处理流程 - ");
+            }
+
+            @Override
+            public void afterProcess(String params, StringBuilder processRecord) {
+                System.out.println("闲鱼活动后置处理流程");
+                processRecord.append("京东活动后置处理流程");
+
+            }
+        };
+    }
+
+    @Override
+    public ModifyExtension1 getModifyExtension1() {
+        return new ModifyExtension1() {
+            @Override
+            public void modify(String params, StringBuilder processRecord) {
+                System.out.println("闲鱼活动优化处理流程1");
+                processRecord.append("闲鱼活动优化处理流程1 - ");
+            }
+        };
+    }
+
+    @Override
+    public ModifyExtension2 getModifyExtension2() {
+        return new ModifyExtension2() {
+            @Override
+            public void modify(String params, StringBuilder processRecord) {
+                System.out.println("闲鱼活动优化处理流程2");
+                processRecord.append("闲鱼活动优化处理流程2 - ");
+            }
+        };
+    }
+}

5b65f601be9dbdb55941bc452d3ac9135a88a6e3
lidongying <lidongying@corp.netease.com>
2018-12-23 00:08:47 +0800

    add Xianyu activity extension

--- dynamic_plugins/src/main/java/com/doing/plugin/extension/xianyu/XianyuExtensionFacade.java
+++ dynamic_plugins/src/main/java/com/doing/plugin/extension/xianyu/XianyuExtensionFacade.java
@@ -18,13 +18,13 @@ public class XianyuExtensionFacade implements ExtensionFacade {
             @Override
             public void beforeProcess(String params, StringBuilder processRecord) {
                 System.out.println("闲鱼活动前置处理流程");
-                processRecord.append("京东活动前置处理流程 - ");
+                processRecord.append("闲鱼活动前置处理流程 - ");
             }
 
             @Override
             public void afterProcess(String params, StringBuilder processRecord) {
                 System.out.println("闲鱼活动后置处理流程");
-                processRecord.append("京东活动后置处理流程");
+                processRecord.append("闲鱼活动后置处理流程");
 
             }
         };

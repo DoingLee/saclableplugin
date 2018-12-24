49b4e4c95cac5a66951348bda89c53742acdf2c9
lidongying <lidongying@corp.netease.com>
2018-12-24 20:49:48 +0800

    test import spring

--- dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension2.java
+++ dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension2.java
@@ -2,14 +2,20 @@ package com.doing.plugin.extension.taobao;
 
 import com.doing.plugin.extension.ModifyExtension2;
 
+import javax.annotation.Resource;
+
+
 /**
  * @author Doing on 19/12/2018 19:26
  */
 public class TaobaoModifyExtension2 implements ModifyExtension2 {
 
+    @Resource(name = "xmlStringBean1")
+    String xmlStringBean1;
+
     @Override
     public void modify(String params, StringBuilder processRecord) {
-        System.out.println("淘宝活动优化处理流程2");
-        processRecord.append("淘宝活动优化处理流程2 - ");
+        System.out.println("淘宝活动优化处理流程2 : " + xmlStringBean1);
+        processRecord.append("淘宝活动优化处理流程2 : " + xmlStringBean1 + " - ");
     }
 }

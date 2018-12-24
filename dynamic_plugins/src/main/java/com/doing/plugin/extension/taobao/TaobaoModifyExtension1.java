892f8013e1ef7f6b876f4422ac8b5bc25ee93b4e
lidongying <lidongying@corp.netease.com>
2018-12-24 18:02:18 +0800

    test modify

--- dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension1.java
+++ dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension1.java
@@ -1,7 +1,7 @@
 package com.doing.plugin.extension.taobao;
 
 import com.doing.plugin.extension.ModifyExtension1;
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 
 /**
  * @author Doing on 19/12/2018 19:26
@@ -10,12 +10,14 @@ public class TaobaoModifyExtension1 implements ModifyExtension1 {
 
     @Override
     public void modify(String params, StringBuilder processRecord) {
-        Gson gson = new Gson();
+//        Gson gson = new Gson();
         String jsonString = "{\"name\":\"活动A\"}";
-        Activity activity = gson.fromJson(jsonString, Activity.class);
+//        Activity activity = gson.fromJson(jsonString, Activity.class);
 
-        System.out.println("淘宝活动优化处理流程1" + activity.getName() + " - ");
-        processRecord.append("淘宝活动优化处理流程1" + activity.getName() + " - ");
+//        System.out.println("淘宝活动优化处理流程1" + activity.getName() + " - ");
+//        processRecord.append("淘宝活动优化处理流程1" + activity.getName() + " - ");
+        System.out.println("淘宝活动优化处理流程1" + jsonString + " - ");
+        processRecord.append("淘宝活动优化处理流程1" + jsonString + " - ");
     }
 
     public static class Activity {

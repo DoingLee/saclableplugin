098d71e0d5b39a9fe436d13137cdb2d25c1c53d9
lidongying <lidongying@corp.netease.com>
2018-12-25 10:13:48 +0800

    test modify

--- dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension2.java
+++ dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension2.java
@@ -17,5 +17,6 @@ public class TaobaoModifyExtension2 implements ModifyExtension2 {
     public void modify(String params, StringBuilder processRecord) {
         System.out.println("淘宝活动优化处理流程2 : " + xmlStringBean1);
         processRecord.append("淘宝活动优化处理流程2 : " + xmlStringBean1 + " - ");
+
     }
 }

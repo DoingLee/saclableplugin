bad0688e4443f5e3cc61a777d31a37108039647f
dongying.ldy <dongying.ldy@alibaba-inc.com>
2019-08-11 18:39:06 +0800

    fix

--- dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension1.java
+++ dynamic_plugins/src/main/java/com/doing/plugin/extension/taobao/TaobaoModifyExtension1.java
@@ -1,10 +1,10 @@
 package com.doing.plugin.extension.taobao;
 
+import com.alibaba.fastjson.JSONObject;
 import com.doing.plugin.domain.ExtensionContext;
 import com.doing.plugin.domain.RequestData;
 import com.doing.plugin.domain.ResponseData;
 import com.doing.plugin.extension.ModifyExtension1;
-import com.google.gson.Gson;
 
 /**
  * @author Doing on 19/12/2018 19:26
@@ -14,13 +14,12 @@ public class TaobaoModifyExtension1 implements ModifyExtension1 {
     @Override
     public void modify(ExtensionContext extensionContext, RequestData requestData, ResponseData responseData) {
         // 测试使用仅在扩展脚本依赖进来的第三方包Gson
-        Gson gson = new Gson();
-        String jsonString = "{\"name\":\"活动A\"}";
-        Activity activity = gson.fromJson(jsonString, Activity.class);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "测试活动名称");
 
-        responseData.addData("Taobao.modify1", "新淘宝活动优化处理流程1=" + activity.getName());
-        extensionContext.getLogger().warn("Taobao.modify1 - 新淘宝活动优化处理流程1={}", activity.getName());
-        extensionContext.debug("Taobao", "新淘宝活动优化处理流程1={}", activity.getName());
+        responseData.addData("Taobao.modify1", "新淘宝活动优化处理流程1=" + jsonObject.getString("name"));
+        extensionContext.getLogger().warn("Taobao.modify1 - 新淘宝活动优化处理流程1={}", jsonObject.getString("name"));
+        extensionContext.debug("Taobao", "新淘宝活动优化处理流程1={}", jsonObject.getString("name"));
     }
 
     public static class Activity {
